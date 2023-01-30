15f4c94ecdf2b11fca5b6070f6f9b5b6708da58d
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-01-30 16:35:36 +0900

    Chore: change release action (#53)

--- .releaserc.js
+++ .releaserc.js
@@ -12,67 +12,124 @@ module.exports = {
       {
         releaseRules: [
           {
-            "type": "Docs",
-            "scope": "README",
-            "release": "patch"
+            type: "Docs",
+            scope: "README",
+            release: "patch"
           },
           {
-            "type": "Fix",
-            "release": "patch"
+            type: "Fix",
+            release: "patch"
           },
           {
-            "type": "Chore",
-            "release": "patch"
+            type: "Chore",
+            release: "patch"
           },
           {
-            "type": "Feat",
-            "release": "minor"
+            type: "Feat",
+            release: "minor"
           },
           {
-            "type": "Perf",
-            "release": "patch"
+            type: "Perf",
+            release: "patch"
           },
           {
-            "type": "Improve",
-            "release": "patch"
+            type: "Improve",
+            release: "patch"
           },
           {
-            "type": "Change",
-            "release": "minor"
+            type: "Change",
+            release: "minor"
           },
           {
-            "type": "Update",
-            "release": false
+            type: "Update",
+            release: false
           },
           {
-            "type": "Add",
-            "release": "minor"
+            type: "Add",
+            release: false
           },
           {
-            "type": "Remove",
-            "release": "minor"
+            type: "Remove",
+            release: "minor"
           },
           {
-            "type": "Rename",
-            "release": false
+            type: "Rename",
+            release: false
           },
           {
-            "type": "Revert",
-            "release": "minor"
+            type: "Revert",
+            release: "minor"
           },
           {
-            "scope": "no-release",
-            "release": false
+            scope: "no-release",
+            release: false
           }
         ]
       }
     ],
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            {
+              type: "Docs",
+              section: "Documentation"
+            },
+            {
+              type: "Fix",
+              section: "Bug fixes"
+            },
+            {
+              type: "Chore",
+              hidden: "Chore"
+            },
+            {
+              type: "Feat",
+              section: "Features"
+            },
+            {
+              type: "Perf",
+              section: "Improvements"
+            },
+            {
+              type: "Improve",
+              section: "Improvements"
+            },
+            {
+              type: "Change",
+              section: "Changes"
+            },
+            {
+              type: "Update",
+              hidden: true
+            },
+            {
+              type: "Add",
+              hidden: true
+            },
+            {
+              type: "Remove",
+              section: "Changes"
+            },
+            {
+              type: "Rename",
+              hidden: true
+            },
+            {
+              type: "Revert",
+              section: "Changes"
+            }
+          ]
+        }
+      }
+    ],
     "@semantic-release/npm",
     [
       "@semantic-release/github",
       {
-        "addReleases": "bottom"
+        addReleases: "bottom"
       }
     ]
   ],
